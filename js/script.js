1a2ee36e38c380efe2b2ce517dc8d03390db004c
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-10-13 22:39:09 +0300

    feat: add slider bg page

--- js/script.js
+++ js/script.js
@@ -49,30 +49,30 @@ function showDate() {
 
 // greeting
 
+function getTimeOfDay() {
+  // Определить текущее время суток
+  const TIMES_OF_DAY = {
+    MORNING: "morning",
+    DAY: "day",
+    EVENING: "evening",
+    NIGHT: "night",
+  }; // времена суток
+  const date = new Date(); // текущая дата + время
+  const hours = date.getHours(); // текущее время в часах
+  if (hours < 6) {
+    return TIMES_OF_DAY.NIGHT;
+  } else if (hours >= 6 && hours < 12) {
+    return TIMES_OF_DAY.MORNING;
+  } else if (hours >= 12 && hours < 18) {
+    return TIMES_OF_DAY.DAY;
+  } else {
+    return TIMES_OF_DAY.EVENING;
+  }
+}
+
 function showGreeting() {
   // функция, которая будет отображать приветствие внутри указанного элемента
   const greetingEl = document.querySelector(".greeting"); // элемент, внутри которого выводится текст приветствия
-
-  function getTimeOfDay() {
-    const TIMES_OF_DAY = {
-      MORNING: "morning",
-      DAY: "day",
-      EVENING: "evening",
-      NIGHT: "night",
-    }; // времена суток
-    const date = new Date(); // текущая дата + время
-    const hours = date.getHours(); // текущее время в часах
-    if (hours < 6) {
-      return TIMES_OF_DAY.NIGHT;
-    } else if (hours >= 6 && hours < 12) {
-      return TIMES_OF_DAY.MORNING;
-    } else if (hours >= 12 && hours < 18) {
-      return TIMES_OF_DAY.DAY;
-    } else {
-      return TIMES_OF_DAY.EVENING;
-    }
-  }
-
   const timeOfDay = getTimeOfDay(); // Определить текущее время суток
   const greetingText = `Good ${timeOfDay}, `; // Сгенерировать приветствие в зависимости от времени суток
   greetingEl.textContent = greetingText; // вывод времени в указанное поле
@@ -86,57 +86,53 @@ inputNameEl.addEventListener("blur", setUserName);
 function setUserName() {
   localStorage.setItem("userName", inputNameEl.value);
 }
-  
+
+// bg page
+
+let randomNum;
+const imagesLength = 20;
+
+function getRandomNum() {
+  const ranNum = Math.floor(Math.random() * imagesLength);
+  randomNum = ranNum === 0 ? 1 : ranNum;
+}
+getRandomNum();
+
+function setBg() {
+  const body = document.querySelector("body"); // outer of slider
+  const timeOfDay = getTimeOfDay(); // текущее время суток
+  const bgNum = `${randomNum}`.padStart(2, "0");
+  body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+}
+setBg();
+
 // slider
 
-// const images = { morning: [], day: [], evening: [], night: [] };
-// const body = document.querySelector("body"); // element for slider
-// const prev = document.querySelector("slide-prev"); // button prev slide
-// const next = document.querySelector("slide-next"); // button next slide
+const slidePrev = document.querySelector(".slide-prev"); // button prev slide
+const slideNext = document.querySelector(".slide-next"); // button next slide
 
-// let index = 0;
+function getSlidePrev() {
+  // уменьшаете рандомное число на единицу, пока оно больше 1. Если результат вычитания равен 1, следующему за ним числу присваиваете значение 20. Внутри этих функций вызываете функцию setBg(), обновляющую фоновое изображение.
+  if (randomNum <= 1) {
+    randomNum = imagesLength;
+  } else {
+    randomNum--;
+  }
+  setBg();
+}
 
-// const activeSlide = (n) => {
-//   for (slide of slides) {
-//     slide.classList.remove("active");
-//   }
-//   slides[n].classList.add("active");
-// };
+function getSlideNext() {
+  // увеличивает рандомное число на 1 пока результат не станет равным 20. Если результат сложения равен 20, следующему за ним числу присваиваете значение 1
+  if (randomNum >= imagesLength) {
+    randomNum = 1;
+  } else {
+    randomNum++;
+  }
+  setBg();
+}
 
-// // отработчик нажатия предидущий слайд
-// const prevSlide = () => {
-//   if (index == slides.length - 1) {
-//     index = 0;
-//     activeSlide(index);
-//   } else {
-//     index++;
-//     activeSlide(index);
-//   }
-// };
-
-// // отработчик нажатия следующий слайд
-// const nextSlide = () => {
-//   if (index == slides.length - 1) {
-//     index = 0;
-//     activeSlide(index);
-//   } else {
-//     index++;
-//     activeSlide(index);
-//   }
-// };
-
-// // по нажатию на кнопку прев срабатывает функция мув лефт
-// prevButton.addEventListener("click", function () {
-//   moveLeft();
-// });
-
-// // по нажатию на кнопку некст срабатывает функция мув райт
-// nextButton.addEventListener("click", function () {
-//   moveRight();
-// });
-
-// body.style.backgroundImage =
-//   "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
+slidePrev.addEventListener("click", getSlidePrev); // отработчик нажатия предидущий слайд
+slideNext.addEventListener("click", getSlideNext); // отработчик нажатия следующий слайд
 
 // //  находим чекбокс который отвечает за автоматику слайдера
 // const sliderCheckbox = document.getElementById("sliderCheckbox");
@@ -148,14 +144,3 @@ function setUserName() {
 //     }, 3000);
 //   }
 // });
-
-// var slideCount = document.querySelector("#slider ul li").length;
-// var slideWidth = document.querySelector("#slider ul li").width();
-// var slideHeight = document.querySelector("#slider ul li").height();
-// var sliderUlWidth = slideCount * slideWidth;
-
-// $("#slider").css({ width: slideWidth, height: slideHeight });
-
-// $("#slider ul").css({ width: sliderUlWidth, marginLeft: -slideWidth });
-
-// $("#slider ul li:last-child").prependTo("#slider ul");
